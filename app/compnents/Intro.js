086804ecca55c7dcaf3c9b4852fa5f697a0562c7
Mass15 <93457138+Mass15@users.noreply.github.com>
2023-12-06 17:12:50 +0530

    error Update Intro.js

--- app/compnents/Intro.js
+++ app/compnents/Intro.js
@@ -6,7 +6,7 @@ const Intro = () => {
   return (
     <section id="intro" >
       <div>
-       <p>I'm <span>Prasanth</span> <br />Web Developer,Code Enthusiast</p>
+       <p>I@apos;m <span>Prasanth</span> <br />Web Developer,Code Enthusiast</p>
        <a href="mailto:prasanthraj608@gmail.com">Contact me</a>
       </div>
     </section>
